5469a0045c0e36ccf8f98fa3575f9a6b0e0e4650
Amat98 <milton.yv98@gmail.com>
2024-08-14 13:21:10 -0500

    aumentando animacion y poniendo la sombra dependiendo de las tareas que existen

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -20,11 +20,24 @@ const Home = () => {
   // console.log(items);
 
   const handleDelete = (index) => {
-    const newItems = items.filter((hol, i) => i !== index);
+    const newItems = items.filter((_, i) => i !== index);
     // const newItems = items.filter((i) => i !== items[index]);
     setItems(newItems);
   };
 
+  function printShadow(arr) {
+    let result =
+      " rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px";
+    let sum = 0;
+    for (let i = 1; i <= arr.length; i++) {
+      let initial = i * 10;
+      sum += 10;
+      result += `, ${initial}px -${initial}px 0px -2px,  rgba(0, 0, 0, 0.2) ${
+        initial + 2
+      }px -${initial - 2}px 5px`;
+    }
+    return result;
+  }
   return (
     <main
       className="text-white d-flex justify-content-center align-items-center flex-column"
@@ -40,10 +53,13 @@ const Home = () => {
         List
       </h1>
       <div
-        className="w-75 mx-auto rounded p-5 bg-white position-relative"
+        className="shadow-container w-75 mx-auto rounded p-5 bg-white position-relative"
         style={{
-          boxShadow:
-            "rgb(38, 57, 77) 0px 20px 100px -10px, rgba(30, 50, 72, 0.4) 0px 5px, rgba(30, 50, 72, 0.3) 0px 10px, rgba(30, 50, 72, 0.2) 0px 15px, rgba(30, 50, 72, 0.1) 0px 20px, rgba(30, 50, 72, 0.05) 0px 25px",
+          // boxShadow:
+          //   "rgb(38, 57, 77) 0px 20px 100px -10px, rgba(30, 50, 72, 0.4) 0px 5px, rgba(30, 50, 72, 0.3) 0px 10px, rgba(30, 50, 72, 0.2) 0px 15px, rgba(30, 50, 72, 0.1) 0px 20px, rgba(30, 50, 72, 0.05) 0px 25px",
+          // boxShadow:
+          //   "#d9d9d9 0px 0px 0px 2px inset, 10px -10px 0px -3px, #d9d9d9 10px -10px, 20px -20px 0px -3px, #d9d9d9 20px -20px, 30px -30px 0px -3px, #d9d9d9 30px -30px, 40px -40px 0px -3px, #d9d9d9 40px -40px",
+          boxShadow: `${printShadow(items)}`,
           maxWidth: "1200px",
         }}
       >
